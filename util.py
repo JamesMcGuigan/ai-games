3f19f2e88afa2390e1273c605892426d29cc203c
JamesMcGuigan <james.mcguigan.github@gmail.com>
2020-04-09 01:11:42 +0100

    fix: make PriorityQueue subclass of Queue

--- util.py
+++ util.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -18,7 +18,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -168,7 +168,7 @@ class Queue:
         "Returns true if the queue is empty"
         return len(self.list) == 0
 
-class PriorityQueue:
+class PriorityQueue(Queue):
     """
       Implements a priority queue data structure. Each inserted item
       has a priority associated with it and the client is usually interested

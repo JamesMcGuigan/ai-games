f47adabde3cf0b639e09bd3a09feaf05b4d4d009
JamesMcGuigan <james.mcguigan.github@gmail.com>
2020-05-17 00:20:55 +0100

    AbstractSolver | move OutputGridSizeSolver into separate file

--- src_james/solvers/AbstractSolver.py
+++ src_james/solvers/AbstractSolver.py
@@ -1,22 +1,17 @@
-from collections import UserDict, defaultdict
-from itertools import chain
-from pprint import pprint
-from typing import Any, Callable, Dict, List, Tuple, Union
+from collections import UserDict
+from typing import Any, Callable, Dict, List, Union
 
 import numpy as np
 
-from src_james.DataModel import Competition, Hashed, Problem, Task
-from src_james.heuristics.Queries import Query
+from src_james.DataModel import Hashed, Problem, Task
 from src_james.solvers.Context import Context
 from src_james.solvers.Rule import Rule
-from src_james.solvers.old.OutputGridSolver import OutputGridSizeTransforms
 
 
 
 class AbstractSolver(Hashed):
     functions = []
     arguments = []
-
 
     def __init__( self ):
         super().__init__()
@@ -64,7 +59,7 @@ class AbstractSolver(Hashed):
 
     def predict( self, problem: Problem ) -> Any:
         """return the predicted problem output"""
-        rule = self.cache.get(task.filename, None)
+        rule = self.cache.get(problem.filename, None)
         assert problem.filename in self.cache
         assert callable(rule)
 
@@ -88,58 +83,3 @@ class AbstractSolver(Hashed):
 
 
 
-class AbstractOutputGridSizeSolver(AbstractSolver):
-    dtype: Tuple[int,int]
-    functions = [
-        OutputGridSizeTransforms.identity,
-        OutputGridSizeTransforms.fixed_size,
-        OutputGridSizeTransforms.ratio,
-    ]
-    arguments = [
-        Query.grid_size_ratio_task,
-        Query.count_nonzero,
-        Query.unique_colors,
-        1/4, 1/3, 1/2, 1, 2, 3, 4,
-    ]
-    def preprocess( self, input: np.ndarray ) -> Any:
-        return input.shape
-
-
-if __name__ == '__main__':
-    solver = AbstractOutputGridSizeSolver()
-    task   = Task('evaluation/68b67ca3.json')
-    rule   = solver.solve(task)
-    # assert rule.name == 'GridSizeIntegerMultiple'
-    # assert rule.args == [(2.0, 2.0)]
-
-    competition  = Competition()
-    solutions    = defaultdict(list)
-    solved_files = { name: [] for name in competition.keys() }
-    error_files  = { name: [] for name in competition.keys() }
-    counts       = defaultdict(int)
-    for name, dataset in competition.items():
-        for task in dataset:
-            #rule  = solver.solve_one(task)
-            rules = solver.solve(task)
-            if not len(rules):
-                error_files[name].append(task.filename)
-            else:
-                solutions[task.filename] += rules
-                solved_files[name].append(task.filename)
-                counts[name] += 1
-
-    print()
-    print('Counts')
-    pprint(counts)
-
-    print('Solutions')
-    for filename, matching_rules in solutions.items():
-        if len(matching_rules):
-            print(filename)
-            for rule in matching_rules: print(rule)
-            print()
-
-    print()
-    print('Errors')
-    for filename in chain(*error_files.values()):
-        print(filename)
---
+++ src_james/solvers/OutputGridSizeSolver.py
@@ -0,0 +1,69 @@
+from collections import defaultdict
+from itertools import chain
+from pprint import pprint
+from typing import Any, Tuple
+
+import numpy as np
+
+from src_james.DataModel import Competition, Task
+from src_james.heuristics.Queries import Query
+from src_james.solvers.AbstractSolver import AbstractSolver
+from src_james.solvers.old.OutputGridSolver import OutputGridSizeTransforms
+
+
+
+class OutputGridSizeSolver(AbstractSolver):
+    dtype: Tuple[int,int]
+    functions = [
+        OutputGridSizeTransforms.identity,
+        OutputGridSizeTransforms.fixed_size,
+        OutputGridSizeTransforms.ratio,
+    ]
+    arguments = [
+        Query.grid_size_ratio_task,
+        Query.count_nonzero,
+        Query.unique_colors,
+        1/4, 1/3, 1/2, 1, 2, 3, 4,
+    ]
+    def preprocess( self, input: np.ndarray ) -> Any:
+        return input.shape
+
+
+if __name__ == '__main__':
+    solver = OutputGridSizeSolver()
+    task   = Task('evaluation/68b67ca3.json')
+    rule   = solver.solve(task)
+    # assert rule.name == 'GridSizeIntegerMultiple'
+    # assert rule.args == [(2.0, 2.0)]
+
+    competition  = Competition()
+    solutions    = defaultdict(list)
+    solved_files = { name: [] for name in competition.keys() }
+    error_files  = { name: [] for name in competition.keys() }
+    counts       = defaultdict(int)
+    for name, dataset in competition.items():
+        for task in dataset:
+            #rule  = solver.solve_one(task)
+            rules = solver.solve(task)
+            if not len(rules):
+                error_files[name].append(task.filename)
+            else:
+                solutions[task.filename] += rules
+                solved_files[name].append(task.filename)
+                counts[name] += 1
+
+    print()
+    print('Counts')
+    pprint(counts)
+
+    print('Solutions')
+    for filename, matching_rules in solutions.items():
+        if len(matching_rules):
+            print(filename)
+            for rule in matching_rules: print(rule)
+            print()
+
+    print()
+    print('Errors')
+    for filename in chain(*error_files.values()):
+        print(filename)

8a6cded59f48b1101b3fb873354e92aacb1b38bb
JamesMcGuigan <james.mcguigan.github@gmail.com>
2020-05-17 03:31:19 +0100

    Context | autoresolve + expand()

--- src_james/core/Context.py
+++ src_james/core/Context.py
@@ -1,24 +1,73 @@
-from collections import UserDict
+from collections import UserDict, UserList
+from typing import Dict, Set
 
 import numpy as np
 
-from src_james.core.DataModel import Problem
+from src_james.core.DataModel import Problem, Task
 
 
 
 class Context(UserDict):
+    excluded_keys  = { 'output', 'solution', 'raw', 'dtype', *dir(UserDict()), *dir(UserList()) }
+    excluded_types = ( str,int,float,tuple,list,set,np.ndarray,type,Task )
+    autoresolve    = ( 'input', 'task', 'filename', 'problemset', 'grids', 'outputs', 'test_or_train', 'inputs' )
+
     def __init__( self, problem: Problem, *args, **kwargs ):
         super().__init__(**kwargs)
-        if len(args) == 0: args = [ problem['input'] ]
+        #if len(args) == 0 and 'input' in problem: args = [ problem['input'] ]
         arg_dict   = { str(index): arg for index,arg in enumerate(args) }
         self.data  = {
             **arg_dict,
-            "input":      problem['input'],
-            "problem":    problem,
-            "problemset": problem.problemset,
-            "task":       problem.task,
+            "problem":  problem,
             **kwargs,
-            }
+        }
+        self._version = 0
+        for field in self.autoresolve:
+            self.__getitem__(field)
+
+        # assert not self.expand(recurse=True)    # auto-resolve (very slow)
+        pass
+
+    def expand( self ) -> Set[str]:
+        start_keys = set(self.data.keys())
+        for key, value in list(self.data.items()):
+            if isinstance(value, self.excluded_types): continue
+            if isinstance(value, (Dict,UserDict)):
+                for name in value:
+                    if name in self.excluded_keys: continue
+                    if name in self.data:          continue
+                    self.data[name] = value[name]
+
+            attrs = set(dir(value)) - set(self.excluded_keys) - set(self.data.keys())
+            for name in attrs:
+                if name.startswith('_'):       continue
+                self.data[name] = getattr(value, name)
+
+        if set(self.data.keys()) - start_keys:
+            self._version += 1
+            self.expand()
+
+        new_keys = set(self.data.keys()) - start_keys
+        return new_keys
+
+    def __lookup(self, key):
+        if key in self.data: return self.data[key]
+
+        for _, value in self.data.items():
+            if hasattr(value, key):
+                return getattr(value, key)
+            if isinstance(value, (Dict,UserDict)) and key in value:
+                return value[key]
+        return None
+
+    def __getitem__(self, key):
+        if key not in self.data:
+            value = self.__lookup(key)
+            if value is not None: self.data[key] = value
+        return self.data.get(key, None)
+
+    def __contains__(self, key):
+        return self.__getitem__(key) is not None
 
     def __str__(self):
         output = {}
--- src_james/core/Symbol.py
+++ src_james/core/Symbol.py
@@ -4,7 +4,7 @@ class Symbol:
     def __new__(cls, name: str):
         name = str(name)
         if name not in cls.instances:
-            self = super(Symbol,cls).__new__(cls)
+            self = super(Symbol, cls).__new__(cls)
             self.__init__(name)
             cls.instances[name] = self
         return cls.instances[name]
@@ -14,3 +14,6 @@ class Symbol:
 
     def __repr__(self):
         return f"Symbol({self.name})"
+
+    def __hash__(self):
+        return hash(self.name)

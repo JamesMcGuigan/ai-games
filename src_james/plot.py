5113534dcabe1c09c60ce08d854c232e076941e1
JamesMcGuigan <james.mcguigan.github@gmail.com>
2020-05-25 09:59:05 +0100

    plot() | add numbers to plot

--- src_james/plot.py
+++ src_james/plot.py
@@ -2,21 +2,37 @@
 from itertools import chain
 
 import matplotlib.pyplot as plt
+import numpy as np
+from fastcache._lrucache import clru_cache
 from matplotlib import colors
 
 # Modified from: https://www.kaggle.com/zaharch/visualizing-all-tasks-updated
 from src_james.core.DataModel import Task
 
 
+@clru_cache()
+def invert_hexcode(hexcode):
+    hexcode = hexcode.replace('#','0x')
+    number  = (16**len(hexcode)-1) - int(hexcode, 16)
+    return hex(number).replace('0x','#')
+
 def plot_one(task, ax, i,train_or_test,input_or_output):
-    cmap = colors.ListedColormap(
-        ['#000000', '#0074D9','#FF4136','#2ECC40','#FFDC00',
-         '#AAAAAA', '#F012BE', '#FF851B', '#7FDBFF', '#870C25'])
+    hexcodes = [
+        '#000000', '#0074D9', '#FF4136', '#2ECC40', '#FFDC00',
+        '#AAAAAA', '#F012BE', '#FF851B', '#7FDBFF', '#870C25',
+    ]
+    inverted_hexcodes = list(map(invert_hexcode,hexcodes))
+    cmap  = colors.ListedColormap(hexcodes)
+    icmap = colors.ListedColormap(inverted_hexcodes)
     norm = colors.Normalize(vmin=0, vmax=9)
 
     try:
         input_matrix = task[train_or_test][i][input_or_output]
+        matrix_size  = np.sqrt(input_matrix.shape[0] * input_matrix.shape[1])
         ax.imshow(input_matrix, cmap=cmap, norm=norm)
+        # DOC: https://stackoverflow.com/questions/33828780/matplotlib-display-array-values-with-imshow
+        for (j,i),label in np.ndenumerate(input_matrix):
+            ax.text(i,j,label,ha='center',va='center', fontsize=50/matrix_size, color='black')
         ax.grid(True,which='both',color='lightgrey', linewidth=0.5)
         ax.set_yticks([x-0.5 for x in range(1+len(input_matrix))])
         ax.set_xticks([x-0.5 for x in range(1+len(input_matrix[0]))])
@@ -58,4 +74,5 @@ def plot_task(task: Task, scale=2):
             plot_one(task_solutions, axs[0,i+j+4+k],k,'solutions','input')
             plot_one(task_solutions, axs[1,i+j+4+k],k,'solutions','output')
 
+    for ax in chain(*axs): ax.axis('off')
     plt.show()

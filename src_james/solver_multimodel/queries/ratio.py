42d3d1baada12b3f700bffed4f415c8778da9edc
JamesMcGuigan <james.mcguigan.github@gmail.com>
2020-05-25 18:46:00 +0100

    ratio | def task_output_grid_shapes(task)

--- src_james/solver_multimodel/queries/ratio.py
+++ src_james/solver_multimodel/queries/ratio.py
@@ -1,5 +1,7 @@
 from itertools import chain
 from typing import List
+from typing import Tuple
+from typing import Union
 
 import numpy as np
 from fastcache._lrucache import clru_cache
@@ -8,44 +10,44 @@ from src_james.core.DataModel import Task
 from src_james.util.np_cache import np_cache
 
 
-@np_cache
-def grid_shape_ratio(grid1, grid2):
+@np_cache()
+def grid_shape_ratio(grid1: np.ndarray, grid2: np.ndarray) -> Tuple[float,float]:
     try:
         return ( grid2.shape[0] / grid1.shape[0], grid2.shape[1] / grid1.shape[1] )
     except:
         return (0, 0)  # For tests
 
-@clru_cache()
-def task_grids(task):
+@clru_cache(maxsize=None)
+def task_grids(task) -> List[np.ndarray]:
     grids = []
     for test_train in ['test','train']:
         for spec in task[test_train]:
             grids += [ spec.get('input',[]), spec.get('output',[]) ]  # tests not gaurenteed to have outputs
     return grids
 
-@clru_cache()
-def task_grid_shapes(task):
+@clru_cache(maxsize=None)
+def task_grid_shapes(task) -> List[Tuple[int,int]]:
     return [ np.array(grid).shape for grid in task_grids(task) ]
 
-@clru_cache()
-def task_grid_max_dim(task):
+@clru_cache(maxsize=None)
+def task_grid_max_dim(task: Task) -> int:
     return max(chain(*task_grid_shapes(task)))
 
-@clru_cache()
-def is_task_shape_ratio_unchanged(task):
+@clru_cache(maxsize=None)
+def is_task_shape_ratio_unchanged(task: Task) -> bool:
     return task_shape_ratios(task) == [ (1,1) ]
 
-@clru_cache()
-def is_task_shape_ratio_consistant(task):
+@clru_cache(maxsize=None)
+def is_task_shape_ratio_consistant(task: Task) -> bool:
     return len(task_shape_ratios(task)) == 1
 
-@clru_cache()
-def is_task_shape_ratio_integer_multiple(task):
+@clru_cache(maxsize=None)
+def is_task_shape_ratio_integer_multiple(task: Task) -> bool:
     ratios = task_shape_ratios(task)
     return all([ isinstance(d, int) or d.is_integer() for d in chain(*ratios) ])
 
-@clru_cache()
-def task_shape_ratios(task: Task) -> List:
+@clru_cache(maxsize=None)
+def task_shape_ratios(task: Task) -> List[Tuple[float,float]]:
     ratios = list(set([
         grid_shape_ratio(problem.get('input',[]), problem.get('output',[]))
         for problem in task['train']
@@ -54,12 +56,29 @@ def task_shape_ratios(task: Task) -> List:
     return ratios
 
 
-@clru_cache()
-def is_task_shape_ratio_consistent(task):
+@clru_cache(maxsize=None)
+def is_task_shape_ratio_consistent(task: Task) -> bool:
     return len(task_shape_ratios(task)) == 1
 
-@clru_cache()
-def is_task_shape_ratio_integer_multiple(task):
+@clru_cache(maxsize=None)
+def is_task_shape_ratio_integer_multiple(task: Task) -> bool:
     ratios = task_shape_ratios(task)
     return all([ isinstance(d, int) or d.is_integer() for d in chain(*ratios) ])
+
+@clru_cache(maxsize=None)
+def task_output_grid_shapes(task: Task) -> List[Tuple[int,int]]:
+    return list(set(
+        problem['output'].shape
+        for problem in task['train']
+        if problem['output'] is not None
+    ))
+
+@clru_cache(maxsize=None)
+def task_output_grid_shape(task) -> Union[Tuple[int,int], None]:
+    grid_sizes = task_output_grid_shapes(task)
+    return len(grid_sizes) == 1 and grid_sizes[0] or None
+
+@clru_cache(maxsize=None)
+def is_task_output_grid_shape_constant(task: Task) -> bool:
+    return bool(task_output_grid_shape(task))
 

bf830897dc1a8b7ff291daeff8ea21fbd4934bf7
JamesMcGuigan <james.mcguigan.github@gmail.com>
2020-05-28 23:44:07 +0100

    XGBSolver.hyperopt.py | update hyperopt search space

--- src_james/solver_multimodel/XGBGridSolver.hyperopt.py
+++ src_james/solver_multimodel/XGBGridSolver.hyperopt.py
@@ -12,6 +12,7 @@ from hyperopt import STATUS_FAIL
 from hyperopt import STATUS_OK
 from hyperopt import tpe
 from hyperopt import Trials
+from hyperopt.pyll import scope
 
 from src_james.core.DataModel import Competition
 from src_james.solver_multimodel.XGBGridSolver import XGBGridSolver
@@ -57,7 +58,6 @@ def hyperopt(param_space, num_eval, points_to_evaluate=None, verbose=True, timeo
 
     def test_function(params={}):
         competition = Competition()
-        dataset     = competition['evaluation']
         try:
             if not debug:
                 with timeout(timeout_seconds):
@@ -65,24 +65,25 @@ def hyperopt(param_space, num_eval, points_to_evaluate=None, verbose=True, timeo
                     solver.verbose = False
                     # competition['test'].apply(solver.solve_dataset)  # Quick for debugging
                     # competition.map(solver.solve_dataset)
-                    dataset.apply(solver.solve_dataset)
+                    competition['training'].apply(solver.solve_dataset)
+                    competition['evaluation'].apply(solver.solve_dataset)
         except Exception as exception:
             # if verbose:
             #     print('Exception:')
             #     print(params)
             #     print(type(exception), exception)
             pass
-        return dataset
+        return competition
 
     def objective_function(params):
-        time_start = time.perf_counter()
-        dataset    = test_function(params)
-        time_taken = time.perf_counter() - time_start
+        time_start  = time.perf_counter()
+        competition = test_function(params)
+        time_taken  = time.perf_counter() - time_start
         score = sum([
-            # competition.score()['training']['correct'],
-            # competition.score()['evaluation']['correct'],
+            competition.score()['training']['correct']/100,
+            competition.score()['evaluation']['correct'],
             # competition.score()['test']['correct'] * 3,
-            dataset.score()['correct'],
+            # dataset.score()['correct'],
             # -int(time_taken)/1000,
         ])
         params = { 'score': score, "time": int(time_taken), **params }
@@ -92,7 +93,6 @@ def hyperopt(param_space, num_eval, points_to_evaluate=None, verbose=True, timeo
             # print(competition)
         status = STATUS_OK if debug or score > 0 else STATUS_FAIL
         return {'loss': -score, 'status': status}
-
 
     points = encode_points_to_evaluate(points_to_evaluate, param_space)
     trials = Trials()
@@ -135,47 +135,47 @@ def hyperopt(param_space, num_eval, points_to_evaluate=None, verbose=True, timeo
 if __name__ == '__main__' and not os.environ.get('KAGGLE_KERNEL_RUN_TYPE', ''):
     # DOCS: https://xgboost.readthedocs.io/en/latest/parameter.html#general-parameters
     param_hyperopt = {
-        # 'n_estimators':      hp.choice('n_estimators', [8]),  # 24+ is optimal, but 8 is quicker once cached
-        # 'n_estimators':      scope.int(hp.quniform('n_estimators',      16, 40, 1)),
-        # 'num_parallel_tree': scope.int(hp.quniform('num_parallel_tree', 1,  40, 1)),
-        # 'max_depth':           scope.int(hp.quniform('max_depth', 5, 25, 1)),
-        # 'min_split_loss':      hp.loguniform('learning_rate',    np.log(1e-3), np.log(1e3)),
-        # 'scale_pos_weight':    hp.loguniform('scale_pos_weight', np.log(1e-3), np.log(1e3)),
-        # 'min_child_weight':    scope.int(hp.quniform('min_child_weight', 0, 4,  1)),
-        # 'max_delta_step':      scope.int(hp.quniform('max_delta_step',   0, 10, 1)),
-        # 'updater':             hp.choice('objective', ['grow_colmaker', 'grow_histmaker', 'grow_local_histmaker', 'grow_skmaker', 'grow_quantile_histmaker', 'grow_gpu_hist', 'refresh','prune']),
-        # 'learning_rate':     hp.loguniform('learning_rate', np.log(0.01), np.log(1)),
+        # 'n_estimators':        hp.choice('n_estimators', [8]),  # 24+ is optimal, but 8 is quicker once cached
+        'n_estimators':        scope.int(hp.quniform('n_estimators',      16, 40, 1)),
+        'num_parallel_tree':   scope.int(hp.quniform('num_parallel_tree', 1,  40, 1)),
+        'max_depth':           scope.int(hp.quniform('max_depth', 5, 25, 1)),
+        'min_split_loss':      hp.loguniform('min_split_loss',   np.log(1e-3), np.log(1e3)),
+        'scale_pos_weight':    hp.loguniform('scale_pos_weight', np.log(1e-3), np.log(1e3)),
+        'min_child_weight':    scope.int(hp.quniform('min_child_weight', 0, 4,  1)),
+        'max_delta_step':      scope.int(hp.quniform('max_delta_step',   0, 10, 1)),
+        'learning_rate':       hp.loguniform('learning_rate', np.log(0.01), np.log(100)),
         # 'colsample_bytree':  hp.uniform('colsample_by_tree', 0.6, 1.0),
-        # 'reg_lambda':        hp.uniform('reg_lambda', 0.0, 1.0),
+        # 'reg_lambda':          hp.uniform('reg_lambda', 0.0, 1.0),
         # 'subsample':         hp.uniform('subsample', 0.3, 1.0),
         # 'max_delta_step':    scope.int(hp.quniform('max_delta_step', 0, 10, 1)),
         # 'num_parallel_tree': scope.int(hp.quniform('num_parallel_tree', 1, 5, 1)),
         # 'base_score':        hp.uniform('base_score', 0.25, 0.75),
+        # 'updater':           hp.choice('objective', ['grow_colmaker', 'grow_histmaker', 'grow_local_histmaker', 'grow_skmaker', 'grow_quantile_histmaker', 'grow_gpu_hist', 'refresh','prune']),
         'sampling_method':     hp.choice('sampling_method', [
             'uniform',
             'gradient_based'
         ]),
         'grow_policy':         hp.choice('grow_policy', ['depthwise', 'lossguide']),
         'tree_method':         hp.choice('tree_method', [
-            'exact',
-            'approx',
-            'hist',
+                'exact',
+            # 'approx',
+            # 'hist',
             # 'gpu_hist'  # score 0
         ]),
         'booster':             hp.choice('booster', [
             'gbtree',
-            'dart',
+            # 'dart',
             # 'gblinear', # WARNING: Parameters: { colsample_bytree, max_delta_step, max_depth, min_child_weight, num_parallel_tree, subsample } might not be used.
         ]),
         'objective':         hp.choice('objective', [
             'reg:squarederror',    # score 9
-            'reg:squaredlogerror', # score 9
-            'reg:logistic',
+            # 'reg:squaredlogerror', # score 9
+            # 'reg:logistic',
             # 'reg:pseudohubererror',  # Unknown objective function: `reg:pseudohubererror`
-            'reg:gamma',
-            'reg:tweedie',       # score 8+
-            'count:poisson',     # score 9
-            'survival:cox',
+            # 'reg:gamma',
+            # 'reg:tweedie',       # score 8+
+            # 'count:poisson',     # score 9
+            # 'survival:cox',
             # 'survival:aft',    # score 8+
             # 'multi:softmax',   # wrong datastructure
             # 'multi:softprob',  # wrong datastructure
@@ -188,33 +188,34 @@ if __name__ == '__main__' and not os.environ.get('KAGGLE_KERNEL_RUN_TYPE', ''):
             # 'rmsle',         # score 0
             # 'mae',           # score 0
             # 'mphe',          # score 0
-            'logloss',         # score 9
+            # 'logloss',         # score 9
             'error',           # score 9
-            'merror',          # score 8+
+            # 'merror',          # score 8+
             # 'mlogloss',      # score 0
             # 'auc',           # score 0
-            'aucpr',           # score 9
-            'ndcg',            # score 9
-            'map',
-            'poisson-nloglik', # score 9
-            'gamma-nloglik',   # score 9
-            'cox-nloglik',
-            'gamma-deviance',
-            'tweedie-nlogli',
+            # 'aucpr',           # score 9
+            # 'ndcg',            # score 9
+            # 'map',
+            # 'poisson-nloglik', # score 9
+            # 'gamma-nloglik',   # score 9
+            # 'cox-nloglik',
+            # 'gamma-deviance',
+            # 'tweedie-nlogli',
             # 'aft-nloglik'     # score 0
         ])
     }
     ### Best keyword results - score = 9
-    points_to_evaluate = [
-        { 'booster': 'dart',   'eval_metric': 'error',          'grow_policy': 'lossguide', 'objective': 'reg:squaredlogerror', 'sampling_method': 'gradient_based', 'tree_method': 'hist'},
-        { 'booster': 'gbtree', 'eval_metric': 'ndcg',           'grow_policy': 'depthwise', 'objective': 'reg:squarederror',    'sampling_method': 'uniform',        'tree_method': 'exact'},
-        { 'booster': 'gbtree', 'eval_metric': 'aucpr',          'grow_policy': 'lossguide', 'objective': 'reg:squarederror',    'sampling_method': 'uniform',        'tree_method': 'exact'},
-        { 'booster': 'gbtree', 'eval_metric': 'ndcg',           'grow_policy': 'lossguide', 'objective': 'count:poisson',       'sampling_method': 'uniform',        'tree_method': 'exact'},
-        { 'booster': 'dart',   'eval_metric': 'gamma-nloglik',  'grow_policy': 'lossguide', 'objective': 'count:poisson',       'sampling_method': 'gradient_based', 'tree_method': 'approx'},
-        { 'booster': 'gbtree', 'eval_metric': 'aucpr',          'grow_policy': 'lossguide', 'objective': 'reg:squarederror',    'sampling_method': 'uniform',        'tree_method': 'approx'},
-        { 'booster': 'dart',   'eval_metric': 'logloss',        'grow_policy': 'depthwise', 'objective': 'reg:squarederror',    'sampling_method': 'gradient_based', 'tree_method': 'approx'},
-        { 'booster': 'dart',   'eval_metric': 'gamma-deviance', 'grow_policy': 'depthwise', 'objective': 'reg:squarederror',    'sampling_method': 'gradient_based', 'tree_method': 'approx'},
-    ]
+    # points_to_evaluate = [
+    #     { 'booster': 'dart',   'eval_metric': 'error',          'grow_policy': 'lossguide', 'objective': 'reg:squaredlogerror', 'sampling_method': 'gradient_based', 'tree_method': 'hist'},
+    #     { 'booster': 'gbtree', 'eval_metric': 'ndcg',           'grow_policy': 'depthwise', 'objective': 'reg:squarederror',    'sampling_method': 'uniform',        'tree_method': 'exact'},
+    #     { 'booster': 'gbtree', 'eval_metric': 'aucpr',          'grow_policy': 'lossguide', 'objective': 'reg:squarederror',    'sampling_method': 'uniform',        'tree_method': 'exact'},
+    #     { 'booster': 'gbtree', 'eval_metric': 'ndcg',           'grow_policy': 'lossguide', 'objective': 'count:poisson',       'sampling_method': 'uniform',        'tree_method': 'exact'},
+    #     { 'booster': 'dart',   'eval_metric': 'gamma-nloglik',  'grow_policy': 'lossguide', 'objective': 'count:poisson',       'sampling_method': 'gradient_based', 'tree_method': 'approx'},
+    #     { 'booster': 'gbtree', 'eval_metric': 'aucpr',          'grow_policy': 'lossguide', 'objective': 'reg:squarederror',    'sampling_method': 'uniform',        'tree_method': 'approx'},
+    #     { 'booster': 'dart',   'eval_metric': 'logloss',        'grow_policy': 'depthwise', 'objective': 'reg:squarederror',    'sampling_method': 'gradient_based', 'tree_method': 'approx'},
+    #     { 'booster': 'dart',   'eval_metric': 'gamma-deviance', 'grow_policy': 'depthwise', 'objective': 'reg:squarederror',    'sampling_method': 'gradient_based', 'tree_method': 'approx'},
+    # ]
+    points_to_evaluate = None
     hyperopt(param_hyperopt, 50, points_to_evaluate=points_to_evaluate, verbose=True, timeout_seconds=180)
 
 ### The problem with running hyperopt on XGBoost for this dataset is that the results are highly variable - 9s score 4-7 on rerun

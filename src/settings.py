93915647f3ee6a2ad55c86aff836bddc39412931
JamesMcGuigan <james.mcguigan.github@gmail.com>
2020-05-29 05:03:14 +0100

    refactor: settings['production']

--- src/settings.py
+++ src/settings.py
@@ -5,7 +5,7 @@ try:    root_dir = pathlib.Path(__file__).parent.parent.absolute()
 except: root_dir = ''
 
 settings = {
-    'production': bool( os.environ.get('KAGGLE_KERNEL_RUN_TYPE') or 'submission' in __file__ )
+    'production': bool( os.environ.get('KAGGLE_KERNEL_RUN_TYPE', False) ) or 'submission' in __file__
 }
 settings = {
     **settings,

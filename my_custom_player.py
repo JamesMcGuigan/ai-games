333fbfaabc64330c26452469919cd05481205d1f
JamesMcGuigan <james.mcguigan.github@gmail.com>
2020-06-03 08:28:23 +0100

    3_Adversarial Search | optimize: refactor for @lru_cache

--- my_custom_player.py
+++ my_custom_player.py
@@ -5,6 +5,7 @@ from itertools import chain
 from operator import itemgetter
 
 from isolation import DebugState, Isolation, play
+from isolation.isolation import Action
 from sample_players import BasePlayer, DataPlayer
 import numpy as np
 
@@ -23,9 +24,20 @@ class CustomPlayer(BasePlayer):
       suitable for using any other machine learning techniques.
 
     - You can pass state forward to your agent on the next turn by assigning
-      any pickleable object to the self.context attribute.
+      any pickleable object to the cls.context attribute.
     **********************************************************************
     """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # self.precache()
+
+    # def precache( self, depth=5 ):
+    #     state = Isolation()
+    #     time_start = time.perf_counter()
+    #     action = self.alphabeta(state, depth=depth)  # precache for first move
+    #     print( 'precache()', type(action), action, int((time.perf_counter() - time_start) * 1000), 'ms' )
+
     def get_action(self, state, verbose=False):
         """ Employ an adversarial search technique to choose an action
         available in the current state calls self.queue.put(ACTION) at least
@@ -50,34 +62,44 @@ class CustomPlayer(BasePlayer):
         #          call self.queue.put(ACTION) at least once before time expires
         #          (the timer is automatically managed for you)
 
-        # Iterative deepening
         time_start = time.perf_counter()
-        self.queue.put( random.choice(state.actions()) )     # backup move incase of early timeout
-        for depth in range(1,100):
-            # action = self.minmax(state,         depth=depth)
-            action = self.alphabeta(state, depth=depth)  # very slow to run for first move
+        action = random.choice(state.actions())
+        self.queue.put( action )     # backup move incase of early timeout
+
+        # The real trick with iterative deepening is caching, which allows us to out-depth the default minimax Agent
+        if verbose: print()
+        for depth in range(1,50):
+            action = self.search(state, depth=depth)
             self.queue.put(action)
-        if verbose:
-            print( type(action), action, int((time.perf_counter() - time_start) * 1000), 'ms' )
+            if verbose: print(depth, end=' ')
+        if verbose: print( depth, type(action), action, int((time.perf_counter() - time_start) * 1000), 'ms' )
 
 
-    def heuristic(self, state):
-        return self.heuristic_area(state)
-        # return self.heuristic_liberties(state)  # won 45%
+    ### Heuristics
 
-    def heuristic_liberties(self, state):
-        own_loc = state.locs[self.player_id]
-        opp_loc = state.locs[1 - self.player_id]
-        own_liberties = self.liberties(state, own_loc)
-        opp_liberties = self.liberties(state, opp_loc)
+    @classmethod
+    @lru_cache(None, typed=True)
+    def heuristic(cls, state, player_id):
+        # return cls.heuristic_area(state, player_id)
+        return cls.heuristic_liberties(state, player_id)  # won 45%
+
+    @classmethod
+    @lru_cache(None, typed=True)
+    def heuristic_liberties( cls, state, player_id ):
+        own_loc = state.locs[player_id]
+        opp_loc = state.locs[1 - player_id]
+        own_liberties = cls.liberties(state, own_loc)
+        opp_liberties = cls.liberties(state, opp_loc)
         return len(own_liberties) - len(opp_liberties)
 
-    def heuristic_area(self, state):
-        own_loc = state.locs[self.player_id]
-        opp_loc = state.locs[1 - self.player_id]
-        own_area = self.area_liberties(state, own_loc)
-        opp_area = self.area_liberties(state, opp_loc)
-        return len(own_area) - len(opp_area)
+    @classmethod
+    @lru_cache(None, typed=True)
+    def heuristic_area( cls, state, player_id, depth=4 ):
+        own_loc = state.locs[player_id]
+        opp_loc = state.locs[1 - player_id]
+        own_area = cls.count_area_liberties(state, own_loc, depth=depth)
+        opp_area = cls.count_area_liberties(state, opp_loc, depth=depth)
+        return own_area - opp_area
 
     @classmethod
     @lru_cache(None, typed=True)
@@ -85,68 +107,90 @@ class CustomPlayer(BasePlayer):
         return state.liberties(cell)
 
     @classmethod
-    @lru_cache(None, typed=True)
-    def area_liberties(cls, state, start_loc, depth=2):  # depth > 1 exceeds 150ms timeout (without caching)
+    @lru_cache(None, typed=True)  # depth > 1 exceeds 150ms timeout (without caching)
+    def count_area_liberties( cls, state, start_loc, depth=np.inf, max=len(Action) * 4 ):  # stop search at 24 liberties
         area      = set()
-        frontier  = set(state.liberties(start_loc))
-        seen      = { start_loc }
-        while len(frontier) and depth > 0:
+        frontier  = { start_loc }
+        seen      = set()
+        while len(frontier) and len(area) < max and depth > 0:
             seen     |= frontier
             frontier |= set(chain(*[ cls.liberties(state, cell) for cell in frontier ]))
             area     |= frontier
             frontier -= seen
             depth    -= 1
-        return area
+        return len(area)
 
 
 
+    ### Search
 
-    def minmax( self, state, depth=np.inf ):
-        def min_value(state, depth):
-            if state.terminal_test(): return state.utility(self.player_id)
-            if depth == 0:            return self.heuristic(state)
-            scores = [
-                max_value(state.result(action), depth-1)
-                for action in state.actions()
-            ]
-            return min(scores) if len(scores) else np.inf
+    def search( self, state, depth ):
+        # return self.minmax(state, depth)
+        return self.alphabeta(state, depth)
 
-        def max_value(state, depth):
-            if state.terminal_test(): return state.utility(self.player_id)
-            if depth == 0:            return self.heuristic(state)
-            scores = [
-                max_value(state.result(action), depth-1)
-                for action in state.actions()
-            ]
-            return max(scores) if len(scores) else -np.inf
 
-        return max(state.actions(), key=lambda action: min_value(state.result(action), depth-1))
+    ### Search: Minmax
 
+    def minmax( self, state, depth ):
+        return max(state.actions(),
+                   key=lambda action: self.minmax_min_value(state.result(action), self.player_id, depth-1))
+
+    @classmethod
+    @lru_cache(None, typed=True)
+    def minmax_min_value(cls, state, player_id, depth):
+        if state.terminal_test(): return state.utility(player_id)
+        if depth == 0:            return cls.heuristic(state, player_id)
+        scores = [
+            cls.minmax_max_value(state.result(action), player_id, depth-1)
+            for action in state.actions()
+        ]
+        return min(scores) if len(scores) else np.inf
+
+    @classmethod
+    @lru_cache(None, typed=True)
+    def minmax_max_value(cls, state, player_id, depth):
+        if state.terminal_test(): return state.utility(player_id)
+        if depth == 0:            return cls.heuristic(state, player_id)
+        scores = [
+            cls.minmax_min_value(state.result(action), player_id, depth-1)
+            for action in state.actions()
+        ]
+        return max(scores) if len(scores) else -np.inf
+
+
+    ### Search: AlphaBeta
 
     def alphabeta( self, state, depth ):
-        def min_value(state, depth, alpha=-np.inf, beta=np.inf):
-            if state.terminal_test(): return state.utility(self.player_id)
-            if depth == 0:            return self.heuristic(state)
-            score = np.inf
-            for action in state.actions():
-                result     = state.result(action)
-                score      = min(score, max_value(result, depth-1, alpha, beta))
-                if score <= alpha: return score
-                beta       = min(beta,score)
-            return score
-
-        def max_value(state, depth, alpha=-np.inf, beta=np.inf):
-            if state.terminal_test(): return state.utility(self.player_id)
-            if depth == 0:            return self.heuristic(state)
-            score = -np.inf
-            for action in state.actions():
-                result     = state.result(action)
-                score      = max(score, max_value(result, depth-1, alpha, beta))
-                if score >= beta: return score
-                alpha      = max(alpha, score)
-            return score
-
         actions = state.actions()
-        scores  = [ min_value(state.result(action), depth=depth-1) for action in actions ]
+        scores  = [
+            self.alphabeta_min_value(state.result(action), player_id=self.player_id, depth=depth-1)
+            for action in actions
+        ]
         score, action = max(zip(scores,actions), key=itemgetter(0))
         return action
+
+    @classmethod
+    @lru_cache(None, typed=True)
+    def alphabeta_min_value(cls, state, player_id, depth, alpha=-np.inf, beta=np.inf):
+        if state.terminal_test(): return state.utility(player_id)
+        if depth == 0:            return cls.heuristic(state, player_id)
+        score = np.inf
+        for action in state.actions():
+            result     = state.result(action)
+            score      = min(score, cls.alphabeta_max_value(result, player_id, depth-1, alpha, beta))
+            if score <= alpha: return score
+            beta       = min(beta,score)
+        return score
+
+    @classmethod
+    @lru_cache(None, typed=True)
+    def alphabeta_max_value(cls, state, player_id, depth, alpha=-np.inf, beta=np.inf):
+        if state.terminal_test(): return state.utility(player_id)
+        if depth == 0:            return cls.heuristic(state, player_id)
+        score = -np.inf
+        for action in state.actions():
+            result     = state.result(action)
+            score      = max(score, cls.alphabeta_min_value(result, player_id, depth-1, alpha, beta))
+            if score >= beta: return score
+            alpha      = max(alpha, score)
+        return score

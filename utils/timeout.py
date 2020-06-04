71444610a8e5b12a656768b927cedfb0baf97e6e
JamesMcGuigan <james.mcguigan.github@gmail.com>
2020-06-04 23:32:40 +0100

    3_Adversarial Search | add utils/@timeout

---
+++ utils/timeout.py
@@ -0,0 +1,24 @@
+# Source: https://www.jujens.eu/posts/en/2018/Jun/02/python-timeout-function/
+import signal
+from contextlib import contextmanager
+
+
+@contextmanager
+def timeout(time):
+    # Register a function to raise a TimeoutError on the signal.
+    signal.signal(signal.SIGALRM, raise_timeout)
+    # Schedule the signal to be sent after ``time``.
+    signal.alarm(time)
+
+    try:
+        yield
+    except TimeoutError:
+        pass
+    finally:
+        # Unregister the signal so it won't be triggered
+        # if the timeout is not reached.
+        signal.signal(signal.SIGALRM, signal.SIG_IGN)
+
+
+def raise_timeout(signum, frame):
+    raise TimeoutError

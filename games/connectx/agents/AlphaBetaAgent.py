e783b1e6c4a6c7894f03f11a4c28664998bcffa3
JamesMcGuigan <james.mcguigan.github@gmail.com>
2020-06-23 00:05:38 +0100

    Revert "ConnectX | AlphaBetaAgent | sign = 1 if player_id != game.player_id else -1"
    This reverts commit a64795e3

--- games/connectx/agents/AlphaBetaAgent.py
+++ games/connectx/agents/AlphaBetaAgent.py
@@ -75,9 +75,8 @@ class AlphaBetaAgent(PersistentCacheAgent):
     def alphabeta_min_value( self, game: KaggleGame, player_id: int, depth: int, alpha=-math.inf, beta=math.inf, endtime=0.0):
         return self.cache_infinite(self._alphabeta_min_value, game, player_id, depth, alpha, beta, endtime)
     def _alphabeta_min_value( self, game: KaggleGame, player_id, depth: int, alpha=-math.inf, beta=math.inf, endtime=0.0 ):
-        sign = 1 if player_id != game.player_id else -1
-        if game.gameover:  return sign * game.heuristic.utility  # score relative to previous player who made the move
-        if depth == 0:     return sign * game.heuristic.score
+        if game.gameover:  return game.heuristic.utility  # score relative to previous player who made the move
+        if depth == 0:     return game.heuristic.score
         scores = []
         score  = math.inf
         for action in game.actions:
@@ -92,9 +91,8 @@ class AlphaBetaAgent(PersistentCacheAgent):
     def alphabeta_max_value( self, game: KaggleGame, player_id: int, depth, alpha=-math.inf, beta=math.inf, endtime=0.0  ):
         return self.cache_infinite(self._alphabeta_max_value, game, player_id, depth, alpha, beta, endtime)
     def _alphabeta_max_value( self, game: KaggleGame, player_id: int, depth, alpha=-math.inf, beta=math.inf, endtime=0.0  ):
-        sign = 1 if player_id != game.player_id else -1
-        if game.gameover:  return sign * game.heuristic.utility  # score relative to previous player who made the move
-        if depth == 0:     return sign * game.heuristic.score
+        if game.gameover:  return game.heuristic.utility  # score relative to previous player who made the move
+        if depth == 0:     return game.heuristic.score
         scores = []
         score  = -math.inf
         for action in game.actions:

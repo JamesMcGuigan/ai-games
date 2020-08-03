c7dd1dd0cbe537325ac69ae73f94764213f9427f
JamesMcGuigan <james.mcguigan.github@gmail.com>
2020-08-03 07:55:04 +0100

    ConnectX | Negamax | sort scores and disable AlphaBeta pruning

--- games/connectx/agents/Negamax/Negamax.py
+++ games/connectx/agents/Negamax/Negamax.py
@@ -4,15 +4,26 @@
 import time
 from struct import Struct
 
+import core.ConnectXBBNN
 from heuristics.BitboardHeuristic import *
 
 
 
-actions = get_all_moves()
-
+configuration = core.ConnectXBBNN.configuration
 
 # @njit
-def negamax( bitboard: np.ndarray, player_id: int, depth: int, max_count=0, alpha=-np.inf, beta=np.inf, action=0, count=0) -> Tuple[int,float,int]:
+def negamax(
+        bitboard: np.ndarray,
+        player_id:  int,
+        depth:      int,
+        max_count:  int   =  0,
+        alpha:      float = -np.inf,
+        beta:       float =  np.inf,
+        action:     int   =  0,
+        count:      int   =  0,
+        sort_scores = np.zeros((configuration.columns,), dtype=np.float),
+        recursing   = False
+    ) -> Tuple[int,float,int, np.ndarray]:
     """
     DOCS: https://en.wikipedia.org/wiki/Negamax
     function negamax(node, depth, α, β, color) is
@@ -29,95 +40,129 @@ def negamax( bitboard: np.ndarray, player_id: int, depth: int, max_count=0, alph
                 break (* cut-off *)
         return value
     """
-    best_action = action  # default action incase of gameover, depth=0 or all scores = -inf
+    best_action  = action  # default action incase of gameover or depth=0
+    scores       = np.zeros((configuration.columns,), dtype=np.float)
 
     if is_gameover(bitboard):
         best_score  = get_utility_inf(bitboard, player_id)
     elif depth == 0:
-        best_score  = bitboard_gameovers_heuristic(bitboard, player_id)
+        best_score = bitboard_gameovers_heuristic(bitboard, player_id)
     else:
-        next_player  = 3 - player_id  # 1 if player_id == 2 else 2
-        actions      = get_legal_moves(bitboard)
-        if best_action not in actions:
-            best_action = np.random.choice(actions)
+        actions = get_legal_moves(bitboard)
+        actions = sorted(actions, key=lambda action: sort_scores[action], reverse=True) # try the best parent moves first
+        if not recursing:
+            actions = [ action for action in actions if sort_scores[action] != -np.inf ]    # auto-exclude losing moves from negamax_deepening()
+            if len(actions) == 0:
+                # All moves are losing, pick the best one by heuristic
+                return negamax(bitboard, player_id, depth=1)
+        if len(actions) == 1:
+            # Short circuit as we have only one valid move
+            best_action = actions[0]
+            best_score  = bitboard_gameovers_heuristic(bitboard, player_id)
+        else:
+            # Recurse until we reach maximum depth
+            next_player = 3 - player_id  # 1 if player_id == 2 else 2
+            best_action  = actions[0]
+            best_score   = -np.inf
+            child_scores = sort_scores
+            for n in range(len(actions)):
+                next_action = actions[n]
+                child_node  = result_action(bitboard, next_action, player_id)
+                child_action, child_score, count, child_scores = negamax(
+                    bitboard    = child_node,
+                    player_id   = next_player,
+                    depth       = depth-1,
+                    max_count   = max_count,
+                    alpha       = -beta,
+                    beta        = -alpha,
+                    action      = next_action,
+                    count       = count+1,
+                    sort_scores = child_scores,
+                    recursing   = True
+                )
+                action_score = -child_score  # Negamax
+                scores[next_action] = action_score
 
-        best_score   = -np.inf
-        scores       = []  # for debugging
-        for n in range(len(actions)):
-            action      = actions[n]
-            child_node  = result_action(bitboard, action, player_id)
-            child_action, child_score, count = negamax(
-                bitboard  = child_node,
-                player_id = next_player,
-                depth     = depth-1,
-                max_count = max_count,
-                alpha     = -beta,
-                beta      = -alpha,
-                action    = action,
-                count     = count+1
-            )
-            child_score = -child_score  # Negamax
-            scores.append(child_score)  # for debugging
+                if action_score > best_score:
+                    best_action = next_action
+                    best_score  = action_score
+                    if best_score == np.inf:
+                        break             # we have found a winning move!
+                if max_count and count >= max_count:
+                    break                 # Deterministic timeout
 
-            if child_score > best_score:
-                best_action = action
-                best_score  = child_score
-            if child_score > alpha:
-                alpha       = child_score
-            if alpha >= beta:
-                break
-            if max_count and count >= max_count:
-                break
+                # BUGFIX: disable AlphaBeta pruning as heuristic is inadmissible and fails foresee future double attacks
+                # if action_score > alpha:
+                #     alpha = action_score  # AlphaBeta pruning - Keep track of best score that other paths have to beat
+                # if alpha >= beta:
+                #     break                 # AlphaBeta pruning - this path is worse than others in the tree, so ignore
 
-    return best_action, best_score, count
+    scores[best_action] = best_score
+    return best_action, best_score, count, scores
 
 
 
-def negamax_deepening( bitboard: np.ndarray, player_id: int, min_depth=1, max_depth=10, depth_step=1, timeout=0.0, verbose_depth=True ) -> int:
+def negamax_deepening(
+        bitboard: np.ndarray,
+        player_id: int,
+        min_depth  = 1,
+        max_depth  = 10,
+        depth_step = 1,
+        timeout    = 0.0,
+        verbose    = 1
+) -> int:
     # The real trick with iterative deepening is caching, which allows us to out-depth the default minimax Agent
-    if verbose_depth: print('\n'+ 'Negamax'.ljust(23) +' | depth:', end=' ', flush=True)
+    if verbose: print('\n'+ 'Negamax'.ljust(23) +' | depth:', end=' ', flush=True)
 
     time_start  = time.perf_counter()
-    best_action = np.random.choice(get_legal_moves(bitboard))
+    legal_moves = get_legal_moves(bitboard)
+    best_action = np.random.choice(legal_moves)
     best_score  = -np.inf
     max_count   = 0
     count       = 0
+    depth       = 0
+    scores      = np.zeros((configuration.columns,), dtype=np.float)
     for depth in range(min_depth, max_depth+1, depth_step):
-        action, score, count = negamax(
-            bitboard  = bitboard,
-            player_id = player_id,
-            max_count = max_count,
-            depth     = depth,
-            count     = count
+        action, score, count, scores = negamax(
+            bitboard    = bitboard,
+            player_id   = player_id,
+            max_count   = max_count,
+            depth       = depth,
+            count       = count,
+            sort_scores = scores
         )
         time_taken     = time.perf_counter() - time_start
-        time_per_count = time_taken / count
+        time_per_count = time_taken / (count + 1)           # BUGFIX: avoid divide by zero
         max_count      = timeout    / time_per_count // 1
 
-        if verbose_depth: print(depth, end=' ', flush=True)
+        if verbose:
+            print(depth, end=' ', flush=True)
+        if verbose >= 2:
+            print(f'Negamax: p{player_id} depth {depth} = action {best_action} (score {best_score:.2f}) | {count} iterations in {time_taken:.3f}s')
+            print('scores', scores.round(2).tolist(),'\n')
+
         if max_count and count >= max_count:
-            break
+            break  # if we didn't complete a full depth search, then ignore the results
         else:
             best_action = action
             best_score  = score
         if abs(best_score) == np.inf:
-            if verbose_depth: print(best_score, end=' ', flush=True)
+            if verbose: print(best_score, end=' ', flush=True)
             break  # terminate iterative deepening on inescapable victory condition
 
-
-        # Update variables for next loop
-        # print(f'time_taken = {time_taken:.2f} | time_per_count = {time_per_count:.8f} | max_count = {max_count:.0f} | count = {count}')
+        non_losing_moves = [ action for action in legal_moves if scores[action] != -np.inf ]
+        if len(non_losing_moves) <= 1:
+            break  # We either have one remaining move, or else all moves are losing - no need to continue searching
 
     time_taken = time.perf_counter() - time_start
-    if verbose_depth:
+    if verbose:
         print(f' = {best_action} ({best_score:.2f}) | {count} iterations in {time_taken:4.2f}s ', flush=True)
-    # print(f'Negamax: p{player_id} depth {depth} = action {action} (score {score:.2f}) | {count} iterations in {time_taken:.3f}s')
+
     return int(best_action)
 
 
 def precompile_numba():
     negamax(bitboard = empty_bitboard(), player_id = 1, depth = 1)
-precompile_numba()
 
 def Negamax():
     def _Negamax_(observation: Struct, _configuration_: Struct) -> int:
@@ -135,17 +180,23 @@ def Negamax():
         bitboard      = list_to_bitboard(listboard)
         move_number   = get_move_number(bitboard)
         is_first_move = int(move_number < 2)
-        timeout       = _configuration_.timeout - safety_time - (first_move_time * is_first_move) - (time.perf_counter() - start_time)
-        action = negamax_deepening(
+        if is_first_move:
+            precompile_numba()
+
+        timeout = _configuration_.timeout - safety_time - (first_move_time * is_first_move) - (time.perf_counter() - start_time)
+        action  = negamax_deepening(
             bitboard   = bitboard,
             player_id  = player_id,
-            min_depth  = 5,
+            min_depth  = 1,
             max_depth  = 100,
-            depth_step = 2,
+            depth_step = 1,
             timeout    = timeout,
-            verbose_depth = True
+            verbose    = 1
         )
-        return int(action)  # kaggle_environments requires a python int, not np.int32
+        if is_first_move:
+            return 3            # BUGFIX: always play the center on the first move | TODO: create opening book
+        else:
+            return int(action)  # kaggle_environments requires a python int, not np.int32
     return _Negamax_
 
 def NegamaxKaggle(observation: Struct, _configuration_: Struct) -> int:
--- games/connectx/tests/test_board_positions.py
+++ games/connectx/tests/test_board_positions.py
@@ -1,5 +1,4 @@
 import pytest
-from kaggle_environments import make
 from kaggle_environments.envs.connectx.connectx import play
 
 from tests.fixtures.agents import agents
@@ -53,6 +52,8 @@ def test_setup_double_attack(observation, configuration, agent_name, agent):
     assert action in [1, 4]
 
 
+
+# NOTE: bitboard_gameovers_heuristic is inadmissible, this AlphaBeta pruning fails to foresee future double attacks
 @pytest.mark.parametrize("agent_name, agent", agents)
 def test_foil_double_attack(observation, configuration, agent_name, agent):
     observation.mark  = 2
@@ -82,6 +83,7 @@ def test_foil_double_attack(observation, configuration, agent_name, agent):
     ]
     action = agent(observation, configuration)
     assert action in [0, 1, 4, 5]
+
 
 
 @pytest.mark.parametrize("agent_name, agent", agents)
@@ -109,6 +111,7 @@ def test_foil_double_attack(observation, configuration, agent_name, agent):
         assert action == 0
 
 
+
 @pytest.mark.parametrize("agent_name, agent", agents)
 def test_single_column(observation, configuration, agent_name, agent):
     configuration.timeout = 60
@@ -119,7 +122,27 @@ def test_single_column(observation, configuration, agent_name, agent):
         2,0,2,2,1,0,2,
         1,0,1,1,2,0,2,
         2,0,2,2,1,0,1,
-        1,0,1,2,2,1,2,
+        1,0,1,2,1,1,2,
     ]
     action = agent(observation, configuration)
     assert action == 5
+
+
+
+@pytest.mark.parametrize("agent_name, agent", agents)
+def test_failed_game_p1_to_block_connect4(observation, configuration, agent_name, agent):
+    observation.mark  = 1
+    observation.board = [
+        0,0,0,0,0,0,0,
+        1,0,0,1,0,0,0,
+        2,0,0,1,0,0,2,
+        1,0,0,2,0,2,2,
+        1,2,0,1,0,2,1,
+        1,1,2,1,0,2,2,
+    ]
+    # Negamax vs negamax played 1 here!!!
+    action = agent(observation, configuration)
+    assert action == 5
+
+
+

1a938249b3ce7d83aaa1f960cf1dc8e54ac397c8
JamesMcGuigan <james.mcguigan.github@gmail.com>
2020-08-16 17:40:56 +0100

    ConnectX | get_endgame_evenodd_columns() | reimplement and test

--- games/connectx/heuristics/BitboardOddEvenHeuristic.py
+++ games/connectx/heuristics/BitboardOddEvenHeuristic.py
@@ -1,4 +1,4 @@
-from fastcache._lrucache import clru_cache
+from fastcache import clru_cache
 
 from core.ConnectXBBNN import *
 
@@ -12,71 +12,62 @@ mask_columns = np.array([
 
 
 
-def get_oddeven_bitboard( bitboard: np.ndarray ) -> Tuple[int,int]:
+def get_evenodd_bitboard( bitboard: np.ndarray ) -> Tuple[int,int]:
     # This is the slow but simple method, loop over the grid and draw the odd/even bit pixels one by one
     column_values = (bitboard[0] & mask_columns[:])
-    odd_columns = [
-        get_odd_column(column_value, col)  # OPTIMIZATION: cache the inner loop
+    even_columns = [
+        get_even_column(column_value, col)  # OPTIMIZATION: cache the inner loop
         for col, column_value in enumerate(column_values)
     ]
-    odd  = sum(odd_columns)
-    even = ~odd & ~bitboard[0] & mask_board
-    return (odd,even)
+    even = sum(even_columns)
+    odd  = ~even & ~bitboard[0] & mask_board
+    return (even, odd)
 
 
 @clru_cache(None)  # size = 2**configuration.rows * configuration.columns == 448
-def get_odd_column( column_bitmask: int, col: int ) -> int:
+def get_even_column( column_bitmask: int, col: int ) -> int:
     column_count = np.count_nonzero( bitcount_mask[:] & column_bitmask )
     column_space = configuration.rows - column_count
 
-    odd = 0
+    even = 0
     for height, row in enumerate(range(column_space-1, -1, -1)):
-        if height % 2 == 1:       # is odd
+        if height % 2 == 0:       # is even
             index = (col + row * configuration.columns)
-            odd  |= (1 << index)  # set odd pixel
-
-    return odd
-
+            even |= (1 << index)  # set even pixel
+    return even
 
 
 
-def get_endgame_player_columns(bitboard: np.ndarray) -> np.ndarray:
+
+def get_endgame_evenodd_columns(bitboard: np.ndarray) -> np.ndarray:
     """
     If all other squares outside the column where played, which player would be forced to play first in this column
     Returns 7x1 int8 array, indicating player number
-
-    TODO: test if output is right way round for p1 = 0 | p2 = 1
-    QUESTION: do we need access to player_id ???
     """
-
-    # Create a bitmask for each column, representing empty squares outside of column
-    outside_of_columns_spaces = np.array([
-        np.sum( mask_board & ~bitboard[0] & ~np.delete(mask_columns[:], col) )
-        for col in range(configuration.columns)
+    column_spaces = (mask_board & ~bitboard[0] & mask_columns[:])
+    column_counts = np.array([
+        np.count_nonzero( bitcount_mask[:] & column_bitmask )
+        for column_bitmask in column_spaces
     ])
-    mask_bitcount = get_bitcount_mask()
-    total_spaces  = np.count_nonzero( bitboard[0] & mask_bitcount[:] )
-    column_spaces = np.array([
-        np.count_nonzero( outside_of_columns_spaces[col] & mask_bitcount[:] )
-        for col in range(configuration.columns)
-    ])
-    output = ((total_spaces - column_spaces) % 2 == 0).astype(np.int8)
-    return output
+    total_spaces   = np.sum(column_counts)
+    player_columns = (total_spaces - column_counts[:]) % 2
+    return player_columns
+
+# def get_column_spaces
 
 
-
-def bitboard_oddeven_heuristic( bitboard: np.ndarray, player_id: int ) -> float:
+def bitboard_evenodd_heuristic( bitboard: np.ndarray, player_id: int ) -> float:
     """
     Returns a heuristic score that attempts to accuratly predict double attacks and forced win lines
     TODO: write unit tests | this code has not even been run yet
     """
-    oddeven        = get_oddeven_bitboard(bitboard)
-    player_columns = get_endgame_player_columns(bitboard)
-    move_number    = get_move_number(bitboard)
+    evenodd         = get_evenodd_bitboard(bitboard)
+    evenodd_columns = get_endgame_evenodd_columns(bitboard)
+    move_number     = get_move_number(bitboard)
     # played = bitboard[0]
     # player = bitboard[1]
-    # odd    = oddeven[0]
-    # even   = oddeven[1]
+    # odd    = evenodd[0]
+    # even   = evenodd[1]
 
     # played_gameovers = gameovers[ bitboard[0] & gameovers == gameovers ]  # filter out any gameovers not in play
     empty_squares     = mask_board  & ~bitboard[0]
@@ -94,10 +85,10 @@ def bitboard_oddeven_heuristic( bitboard: np.ndarray, player_id: int ) -> float:
     p1_is_multibit   = p1_multibit.astype(bool)  & ~p1_is_singlebit
     p2_is_multibit   = p1_multibit.astype(bool)  & ~p2_is_singlebit
 
-    p1_odd_gameovers  = gameovers[:] == gameovers[:] & (p1_tokens | oddeven[0])
-    p1_even_gameovers = gameovers[:] == gameovers[:] & (p1_tokens | oddeven[1])
-    p2_odd_gameovers  = gameovers[:] == gameovers[:] & (p2_tokens | oddeven[0])
-    p2_even_gameovers = gameovers[:] == gameovers[:] & (p2_tokens | oddeven[1])
+    p1_odd_gameovers  = gameovers[:] == gameovers[:] & (p1_tokens | evenodd[0])
+    p1_even_gameovers = gameovers[:] == gameovers[:] & (p1_tokens | evenodd[1])
+    p2_odd_gameovers  = gameovers[:] == gameovers[:] & (p2_tokens | evenodd[0])
+    p2_even_gameovers = gameovers[:] == gameovers[:] & (p2_tokens | evenodd[1])
     p1_odd_count      = np.count_nonzero(p1_odd_gameovers)
     p1_even_count     = np.count_nonzero(p1_even_gameovers)
     p2_odd_count      = np.count_nonzero(p1_odd_gameovers)
@@ -108,7 +99,7 @@ def bitboard_oddeven_heuristic( bitboard: np.ndarray, player_id: int ) -> float:
     reward_gameover        = np.inf
     reward_single_token    = 0.01
     reward_multi           = 0.1
-    reward_oddeven_pair    = 1.0
+    reward_evenodd_pair    = 1.0
     reward_odd_with_player = 1.0
     reward_odd_with_column = 1.0
 
@@ -121,8 +112,8 @@ def bitboard_oddeven_heuristic( bitboard: np.ndarray, player_id: int ) -> float:
     p2_score += reward_single_token * np.count_nonzero(p2_is_singlebit)
     p1_score += reward_multi        * np.count_nonzero(p1_is_multibit)
     p2_score += reward_multi        * np.count_nonzero(p2_is_multibit)
-    p1_score += reward_oddeven_pair * p1_pair_count
-    p2_score += reward_oddeven_pair * p2_pair_count
+    p1_score += reward_evenodd_pair * p1_pair_count
+    p2_score += reward_evenodd_pair * p2_pair_count
 
     player_bonus = reward_odd_with_player * np.max([ p2_even_count - p2_pair_count, 0 ])
     if move_number % 2 == 0:  # player 1 to move, score from perspective of p2, evens are a win
--- games/connectx/heuristics/BitboardOddEvenHeuristic_test.py
+++ games/connectx/heuristics/BitboardOddEvenHeuristic_test.py
@@ -1,42 +1,59 @@
-from core.ConnectXBBNN import *
-from heuristics.BitboardOddEvenHeuristic import get_oddeven_bitboard
+from heuristics.BitboardOddEvenHeuristic import *
 
 
-def test_get_oddeven_bitboard__empty():
-    """ The oddeven of an empty bitboard should be a checkerboard pattern """
+def test_get_evenodd_bitboard__empty():
+    """ The evenodd of an empty bitboard should be a checkerboard pattern """
     bitboard      = empty_bitboard()
-    oddeven       = get_oddeven_bitboard(bitboard)
+    evenodd       = get_evenodd_bitboard(bitboard)
     bitcount_mask = get_bitcount_mask()
-    bitcount_odd  = np.count_nonzero(bitcount_mask[:] & oddeven[0])
-    bitcount_even = np.count_nonzero(bitcount_mask[:] & oddeven[1])
-    print( f"({bitcount_odd}) {oddeven[0]:042b} == {oddeven[1]:042b} ({bitcount_even})" )
-    assert oddeven[0]    == ~oddeven[1] & mask_board  # odd and even should be the inverse of each other for empty board
+    bitcount_odd  = np.count_nonzero(bitcount_mask[:] & evenodd[0])
+    bitcount_even = np.count_nonzero(bitcount_mask[:] & evenodd[1])
+    print( f"({bitcount_odd}) {evenodd[0]:042b} == {evenodd[1]:042b} ({bitcount_even})" )
+    assert evenodd[0]    == ~evenodd[1] & mask_board  # odd and even should be the inverse of each other for empty board
     assert bitcount_odd  == bitcount_even
     assert bitcount_odd  == (configuration.columns * configuration.rows)//2
     assert bitcount_even == (configuration.columns * configuration.rows)//2
     for row in range(configuration.rows):
-        assert      oddeven[0] & (1 << row)    # top row should be odd
-        assert not( oddeven[1] & (1 << row) )  # top row should not be even
+        assert not( evenodd[0] & (1 << row) )  # top row should not be even
+        assert      evenodd[1] & (1 << row)    # top row should be odd
+
 
         bottom_row_offset = (configuration.rows-1) * configuration.columns
-        assert not ( oddeven[0] & (1 << (row + bottom_row_offset)) )  # bottom row should not be odd
-        assert       oddeven[1] & (1 << (row + bottom_row_offset))    # bottom row should be even
+        assert       evenodd[0] & (1 << (row + bottom_row_offset))    # bottom row should be even
+        assert not ( evenodd[1] & (1 << (row + bottom_row_offset)) )  # bottom row should not be odd
 
 
-def test_get_oddeven_bitboard__columns():
+def test_get_evenodd_bitboard__triangle():
     """ Draw a 3x3 triangle in the corner, and check it produces diagonal odd/even lines and exclude played squares """
     bitboard = empty_bitboard()
     for action in [0,0,0,1,1,2]:  # 3x3 triangle
         bitboard = result_action(bitboard, action, 1)
-    oddeven = get_oddeven_bitboard(bitboard)
+    evenodd = get_evenodd_bitboard(bitboard)
 
-    assert bitboard[0] & oddeven[0] == 0  # oddeven should exclude played squares
-    assert bitboard[0] & oddeven[1] == 0  # oddeven should exclude played squares
+    assert bitboard[0] & evenodd[0] == 0  # evenodd should exclude played squares
+    assert bitboard[0] & evenodd[1] == 0  # evenodd should exclude played squares
 
     for row in range(4):
-        assert not( oddeven[0] & (1 << row + row * configuration.columns) )   # top down diagonals should be odd
-        assert      oddeven[1] & (1 << row + row * configuration.columns)     # top down diagonals should not be even
+        assert      evenodd[0] & (1 << row + row * configuration.columns)     # top down diagonals should not be even
+        assert not( evenodd[1] & (1 << row + row * configuration.columns) )   # top down diagonals should be odd
 
     for row in range(4, configuration.columns):
-        assert      oddeven[0] & (1 << row)    # top row (not above triangle) should be odd
-        assert not( oddeven[1] & (1 << row) )  # top row (not above triangle) should not be even
+        assert not( evenodd[0] & (1 << row) )  # top row (not above triangle) should not be even
+        assert      evenodd[1] & (1 << row)    # top row (not above triangle) should be odd
+
+
+
+
+def test_get_endgame_evenodd_columns__empty():
+    bitboard       = empty_bitboard()
+    player_columns = get_endgame_evenodd_columns(bitboard)
+    assert player_columns.tolist() == [ 0 ] * configuration.columns
+
+
+def test_get_endgame_player_columns__triangle():
+    bitboard = empty_bitboard()
+    for action in [0,0,0,1,1,2]:  # 3x3 triangle
+        bitboard = result_action(bitboard, action, 1)
+
+    player_columns = get_endgame_evenodd_columns(bitboard)
+    assert player_columns.tolist() == [ 1, 0, 1, 0, 0, 0, 0 ]

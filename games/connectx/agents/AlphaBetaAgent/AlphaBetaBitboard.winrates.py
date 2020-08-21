6eb2f05022db5022f10017dea436c26d947325f2
JamesMcGuigan <james.mcguigan.github@gmail.com>
2020-08-21 22:25:21 +0100

    ConnectX | winrates | joblib Parallel()

--- games/connectx/agents/AlphaBetaAgent/AlphaBetaBitboard.winrates.py
+++ games/connectx/agents/AlphaBetaAgent/AlphaBetaBitboard.winrates.py
@@ -1,53 +1,79 @@
 #!/usr/bin/env python3
 # seq 5 | parallel --ungroup --jobs 2 agents/AlphaBetaAgent/AlphaBetaBitboard.winrates.py -r 2 -t 5
 import argparse
-import gc
+import multiprocessing
 import time
 
+import numpy as np
+from joblib import delayed
+from joblib import Parallel
 from kaggle_environments import make
 
-from agents.AlphaBetaAgent.AlphaBetaAgent import AlphaBetaAgent
 from agents.AlphaBetaAgent.AlphaBetaBitboard import AlphaBetaBitboard
+from agents.AlphaBetaAgent.AlphaBetaBitboardEvenOdd import AlphaBetaBitboardEvenOdd
+from heuristics.BitboardOddEvenHeuristic import bitboard_evenodd_heuristic
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-r', '--rounds',  type=int, default=10)
-parser.add_argument('-t', '--timeout', type=int, default=5)  # Half time for faster benchmarks
+parser.add_argument('-r', '--rounds',  type=int, default=60)
+parser.add_argument('-t', '--timeout', type=int, default=1 + 2)  # 1s for faster benchmarks | AlphaBetaBitboard has depth=4 opening
 parser.add_argument('argv', nargs=argparse.REMAINDER)
 argv = parser.parse_args()
 
 
 env = make("connectx", debug=True)
-env.configuration.timeout = 8   # Half normal timer for quicker tests
+env.configuration.timeout = argv.timeout   # Half normal timer for quicker tests
 observation   = env.state[0].observation
 configuration = env.configuration
 
 rounds   = argv.rounds
 agent    = AlphaBetaBitboard
-opponent = AlphaBetaAgent
-kwargs = {
-    "verbose_depth":    False,
-}
+opponent = AlphaBetaBitboardEvenOdd
+
 
 scores = { agent.__name__: 0, opponent.__name__: 0 }
-for round in range(rounds):
-    gc.collect()
+def run_round(agent, opponent, round=0):
+    kwargs = {
+        "verbose_depth":  False,
+    }
+    agent_kwargs    = { **kwargs }
+    opponent_kwargs = {
+        "heuristic_fn": bitboard_evenodd_heuristic(
+            reward_gameover        = np.inf,
+            reward_single_token    = 0.1,
+            reward_multi           = 1,
+            reward_evenodd_pair    = 4,
+            reward_odd_with_player = 0.5,
+            reward_odd_with_column = 1,
+        ),
+        **kwargs
+    }
+
     env = make("connectx", debug=True)
     env.configuration.timeout = argv.timeout
     time_start = time.perf_counter()
     if round % 2 == 0:
-        env.run([agent.agent(**kwargs), opponent.agent(**kwargs)])
+        agent_order = [ agent.agent(**agent_kwargs), opponent.agent(**opponent_kwargs) ]
+        env.run(agent_order)
         if   env.state[0].reward ==  1: scores[agent.__name__]    += 1
         elif env.state[0].reward == -1: scores[opponent.__name__] += 1
         else: scores[agent.__name__] += 0.5; scores[opponent.__name__] += 0.5;
     else:
-        env.run([opponent.agent(**kwargs), agent.agent(**kwargs)])
+        agent_order = [ opponent.agent(**opponent_kwargs), agent.agent(**agent_kwargs) ]
+        env.run(agent_order)
         if   env.state[1].reward ==  1: scores[agent.__name__]    += 1
         elif env.state[1].reward == -1: scores[opponent.__name__] += 1
         else: scores[agent.__name__] += 0.5; scores[opponent.__name__] += 0.5;
     time_taken = time.perf_counter() - time_start
-    print(f'{time_taken:6.2f}s', scores)
+    print(f'{time_taken:6.1f}s', scores)
+    return scores
 
-wins    = scores[agent.__name__]
+n_jobs = rounds if rounds < multiprocessing.cpu_count() * 2 else multiprocessing.cpu_count()
+all_scores = Parallel(n_jobs=n_jobs)([
+    delayed(run_round)(agent, opponent, round)
+    for round in range(rounds)
+])
+
+wins    = sum([ scores[agent.__name__] for scores in all_scores ])
 winrate = 100 * wins / rounds
 print(f'{wins:.1f}/{rounds:.0f} = {winrate:3.0f}% winrate {agent.__name__} vs {opponent.__name__}')
 

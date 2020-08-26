f88468dcdfd5955c4f332c41980324be2a6539b3
JamesMcGuigan <james.mcguigan.github@gmail.com>
2020-08-26 16:59:00 +0100

    ConnectX | tests | agents | add MontyCarloBitsquares() + MontyCarloOddEven()

--- games/connectx/tests/fixtures/agents.py
+++ games/connectx/tests/fixtures/agents.py
@@ -1,20 +1,24 @@
 
 from agents.AlphaBetaAgent.AlphaBetaAgent import AlphaBetaAgent
 from agents.AlphaBetaAgent.AlphaBetaBitboard import AlphaBetaBitboard
-from agents.AlphaBetaAgent.AlphaBetaBitboardEvenOdd import AlphaBetaBitboardEvenOdd
 from agents.AlphaBetaAgent.AlphaBetaBitsquares import AlphaBetaBitsquares
+from agents.AlphaBetaAgent.AlphaBetaOddEven import AlphaBetaOddEven
+from agents.MontyCarlo.MontyCarloBitsquares import MontyCarloBitsquares
 from agents.MontyCarlo.MontyCarloHeuristic import MontyCarloHeuristic
+from agents.MontyCarlo.MontyCarloOddEven import MontyCarloOddEven
 from agents.MontyCarlo.MontyCarloPure import MontyCarloPure
 from agents.Negamax.Negamax import Negamax
 
 agents = [
     # ('AntColonyTreeSearch',      AntColonyTreeSearch()),   # Unit tests currently broken
+    ('MontyCarloBitsquares',     MontyCarloBitsquares()),
+    ('MontyCarloOddEven',        MontyCarloOddEven()),
     ('MontyCarloHeuristic',      MontyCarloHeuristic()),
     ('MontyCarloPure',           MontyCarloPure()),
     ('Negamax',                  Negamax()),
     ('AlphaBetaAgent',           AlphaBetaAgent.agent()),
     ('AlphaBetaBitboard',        AlphaBetaBitboard.agent()),
-    ('AlphaBetaBitboardEvenOdd', AlphaBetaBitboardEvenOdd.agent()),
+    ('AlphaBetaBitboardEvenOdd', AlphaBetaOddEven.agent()),
     ('AlphaBetaBitsquares',      AlphaBetaBitsquares.agent()),
 ]
 

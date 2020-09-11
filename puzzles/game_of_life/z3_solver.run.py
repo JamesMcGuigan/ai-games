462894b909d80f5da2b4265db46caaf672655e32
JamesMcGuigan <james.mcguigan.github@gmail.com>
2020-09-12 00:09:43 +0100

    Game Of Life | z3_solver.run.py | document run command

--- puzzles/game_of_life/z3_solver.run.py
+++ puzzles/game_of_life/z3_solver.run.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+# PYTHONUNBUFFERED=1 time ./z3_solver.run.py | tee -a submission.log
 
 from datasets import test_df
 from z3_solver import solve_dataframe
+
 
 if __name__ == '__main__':
     solve_dataframe(test_df, save='submission.csv')

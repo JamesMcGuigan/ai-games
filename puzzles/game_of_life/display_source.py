8d09300437189e52633911b702f17ebb125b17e3
JamesMcGuigan <james.mcguigan.github@gmail.com>
2020-09-11 17:18:07 +0100

    Game Of Life | display_source.py

---
+++ puzzles/game_of_life/display_source.py
@@ -0,0 +1,17 @@
+import IPython
+
+def display_source(code):
+    def _jupyterlab_repr_html_(self):
+        from pygments import highlight
+        from pygments.formatters import HtmlFormatter
+
+        fmt = HtmlFormatter(style='tango')  # https://overiq.com/pygments-tutorial/#style
+        style = "<style>{}\n{}</style>".format(
+            fmt.get_style_defs(".output_html"), fmt.get_style_defs(".jp-RenderedHTML")
+        )
+        return style + highlight(self.data, self._get_lexer(), fmt)
+
+    # Replace _repr_html_ with our own version that adds the 'jp-RenderedHTML' class
+    # in addition to 'output_html'.
+    IPython.display.Code._repr_html_ = _jupyterlab_repr_html_
+    return IPython.display.Code(data=code, language="python3")

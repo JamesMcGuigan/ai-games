e2609124f1a1737f6475282ae78a00bed4f07674
JamesMcGuigan <james.mcguigan.github@gmail.com>
2020-10-02 17:24:00 +0100

    Game Of Life | GameOfLifeHardcoded() | tests for LeakyReLU + ReLU6 implementations

---
+++ puzzles/game_of_life/neural_networks/tests/test_GameOfLifeHardcoded.py
@@ -0,0 +1,62 @@
+import numpy as np
+import pytest
+
+from neural_networks.GameOfLifeHardcoded import GameOfLifeHardcodedLeakyReLU
+from neural_networks.GameOfLifeHardcoded import GameOfLifeHardcodedReLU6
+
+models = [
+    GameOfLifeHardcodedLeakyReLU(),
+    GameOfLifeHardcodedReLU6(),
+]
+
+@pytest.mark.parametrize("boards", [
+    [[ 0,0,0,0,0,
+       0,0,1,0,0,
+       0,1,1,1,0,
+       0,0,1,0,0,
+       0,0,0,0,0,],
+     [ 0,0,0,0,0,
+       0,1,1,1,0,
+       0,1,0,1,0,
+       0,1,1,1,0,
+       0,0,0,0,0,]
+     ],
+])
+@pytest.mark.parametrize("model", models)
+def test_GameOfLifeHardcoded_sequences(model, boards):
+    boards = np.array(boards).reshape((-1,5,5))
+    for n in range(len(boards)-1):
+        input    = boards[n]
+        expected = boards[n+1]
+        output   = model.predict(input)
+        assert np.array_equal( output, expected )  # assert 100% accuracy
+
+
+
+@pytest.mark.parametrize("board,delta", [
+    ([ 0,0,0,0,0,
+       0,0,0,0,0,
+       0,1,1,1,0,
+       0,0,0,0,0,
+       0,0,0,0,0,], 2),
+
+    ([ 0,0,0,0,0,
+       0,1,1,0,0,
+       0,1,1,0,0,
+       0,0,0,0,0,
+       0,0,0,0,0,], 1),
+
+    ([ 0,0,0,0,0,
+       0,0,0,1,0,
+       0,1,0,1,0,
+       0,0,1,1,0,
+       0,0,0,0,0,], 20),
+])
+@pytest.mark.parametrize("model", models)
+def test_GameOfLifeHardcoded_repeating_patterns(model, board, delta):
+    board    = np.array(board).reshape(5,5)
+    expected = board
+    output   = board
+    for n in range(delta):
+        output = model.predict(output)
+    assert np.array_equal( output, expected )  # assert 100% accuracy

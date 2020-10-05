e0fd1a213ba48a6e2803561ccca98b37f6a15143
JamesMcGuigan <james.mcguigan.github@gmail.com>
2020-10-05 11:02:29 +0100

    Game Of Life | neural networks | refactor stats script

--- puzzles/game_of_life/neural_networks/hardcoded/logs/GameOfLifeForward.stats.sh
+++ puzzles/game_of_life/neural_networks/hardcoded/logs/GameOfLifeForward.stats.sh
@@ -1,7 +1,12 @@
+#!/usr/bin/env bash
+cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" || exit
+
 for N in 1 1N 2 2N 4 128; do
-  rgrep "Finished Training: GameOfLifeForward_$N " |
+  TOTAL=$(find ./ -name "GameOfLifeForward_$N.*.log" | wc -l)
+  find ./ -name "GameOfLifeForward_$N.*.log" | grep "^Finished Training: GameOfLifeForward_$N " ./*.log |
   awk '
-    NR == 1 { name=$3; min_epochs = int($5); max_epochs = int($5); min_time = int($8); max_time = int($8); count = 0 }
+    NR == 1 { count = 0; total='${TOTAL}' }
+    NR == 1 { name=$3; min_epochs = int($5); max_epochs = int($5); min_time = int($8); max_time = int($8); }
     NR > 1 && $5 < min_epochs { min_epochs = int($5) }
     NR > 1 && $5 > max_epochs { max_epochs = int($5) }
     NR > 1 && $8 < min_time   { min_time   = int($8) }
@@ -9,9 +14,11 @@ for N in 1 1N 2 2N 4 128; do
     { avg_epochs += $5; avg_time += $8; count += 1; }
     END { avg_epochs = int(avg_epochs / count) }
     END { avg_time   = int(avg_time   / count) }
-    END { print name" | "count"/10 successes | epochs = "min_epochs" / "avg_epochs" / "max_epochs" | time seconds = "min_time" / "avg_time" / "max_time" (min/avg/max)" }
+    END { printf "%-21s | %2d/%2d successes | epochs = %4d / %4d / %5d | time seconds = %3d / %3d / %3d (min/avg/max)\n",
+                  name,   count, total,       min_epochs, avg_epochs, max_epochs,   min_time, avg_time, max_time }
   ';
 done
+
 
 # GameOfLifeForward_1   |  9/10 successes | epochs = 2254 / 5313 / 10318 | time seconds = 183 / 420 / 807 (min/avg/max)
 # GameOfLifeForward_1N  |  6/10 successes | epochs = 3376 / 4771 /  7097 | time seconds = 262 / 370 / 552 (min/avg/max)

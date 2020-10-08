5c7eeb2b24b49ff0e5f165653e5cd1e746886566
JamesMcGuigan <james.mcguigan.github@gmail.com>
2020-10-08 23:16:33 +0100

    Game Of Life | neural networks | GameOfLifeForward.sh | reduce timer to 30m

--- puzzles/game_of_life/neural_networks/hardcoded/logs/GameOfLifeForward.sh
+++ puzzles/game_of_life/neural_networks/hardcoded/logs/GameOfLifeForward.sh
@@ -5,6 +5,6 @@ cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")/../../../"
 parallel -k --ungroup --jobs 6 "
 (
   rm -vf ./neural_networks/models/GameOfLifeForward_{1}.pth;
-  PYTHONPATH='.' PYTHONUNBUFFERED=1 time -p timeout 60m python3 ./neural_networks/hardcoded/GameOfLifeForward_{1}.py
+  PYTHONPATH='.' PYTHONUNBUFFERED=1 time -p timeout 30m python3 ./neural_networks/hardcoded/GameOfLifeForward_{1}.py
 ) 2>&1 | tee ./neural_networks/hardcoded/logs/GameOfLifeForward_{1}.{2}.log
 " ::: 11N 1N 1 2N 2 4 128 ::: {1..10}

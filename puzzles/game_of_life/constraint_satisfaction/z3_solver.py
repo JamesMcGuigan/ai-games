7f3dd579493d1d87f75711bfead066b9cf9cea43
JamesMcGuigan <james.mcguigan.github@gmail.com>
2020-10-09 23:40:48 +0100

    Game Of Life | constraint satisfaction | restore get_static_board_constraint()

--- puzzles/game_of_life/constraint_satisfaction/z3_solver.py
+++ puzzles/game_of_life/constraint_satisfaction/z3_solver.py
@@ -9,6 +9,7 @@ from constraint_satisfaction.z3_constraints import get_game_of_life_ruleset
 from constraint_satisfaction.z3_constraints import get_image_segmentation_csv
 from constraint_satisfaction.z3_constraints import get_initial_board_constraint
 from constraint_satisfaction.z3_constraints import get_no_empty_boards_constraint
+from constraint_satisfaction.z3_constraints import get_static_board_constraint
 from constraint_satisfaction.z3_constraints import get_t_cells
 from constraint_satisfaction.z3_constraints import get_zero_point_constraint
 from constraint_satisfaction.z3_utils import solver_to_numpy_3d
@@ -34,7 +35,7 @@ def game_of_life_solver(board: np.ndarray, delta: int, idx: int, timeout=0, verb
     # z3_solver.maximize( get_static_pattern_score(t_cells) )
 
     for constraint in [
-        # lambda: get_static_board_constraint(t_cells, board),
+        lambda: get_static_board_constraint(t_cells, board),
         # lambda: get_image_segmentation_solver_constraint(t_cells, board, delta), # pickle file is too large for github
         lambda: get_image_segmentation_csv(t_cells, idx),
         lambda: [],

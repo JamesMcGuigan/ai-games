09e9304774d6cc1e74f604657beaa9a1e77a0fa5
JamesMcGuigan <james.mcguigan.github@gmail.com>
2021-01-31 09:35:57 +0000

    Rock Paper Scissors | Random Seed Search | run for IrrationalSearchAgent

--- games/rock-paper-scissors/rng/RandomSeedSearch.run.py
+++ games/rock-paper-scissors/rng/RandomSeedSearch.run.py
@@ -5,12 +5,15 @@ from humanize import precisedelta
 from joblib import delayed, Parallel
 from kaggle_environments import evaluate
 
+from rng.IrrationalAgent import IrrationalAgent
+from rng.IrrationalSearchAgent import IrrationalSearchAgent
 from rng.random_agent_unseeded import random_agent_unseeded
-from rng.RandomSeedSearch import random_seed_search_agent
+from rng.RandomSeedSearch import random_seed_search_agent, RandomSeedSearch
 
 time_start = time.perf_counter()
 
-agents  = [ random_seed_search_agent, random_agent_unseeded ]
+agents  = [ RandomSeedSearch(), random_agent_unseeded ]
+agents  = [ IrrationalSearchAgent(), IrrationalAgent(name='pi') ]
 results = Parallel(-1)(
     delayed(evaluate)(
         "rps",
@@ -19,15 +22,15 @@ results = Parallel(-1)(
             "episodeSteps": 1000,
             # "actTimeout":   1,
         },
-        num_episodes=8,
+        num_episodes=1,
         debug=False
     )
-    for _ in range(10)
+    for _ in range(80)
 )
 results = np.array(results).reshape((-1,2))
 
 time_taken = time.perf_counter() - time_start
-print([ agent.__name__ for agent in agents ])
+print([ getattr(agent, '__name__', agent.__class__.__name__) for agent in agents ])
 print('winrate', [ np.sum(results[:,0] > results[:,1]+20), np.sum(results[:,0]+20 < results[:,1]) ], '/', len(results))
 print('scores ', np.sum(results, axis=0).round(2))
 print('std    ', np.std(results, axis=0).round(2))
